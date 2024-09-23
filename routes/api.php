c10539921a412535b67f2a91e06cc4efaca6d566
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-09-23 09:34:13 +0700

    Refactor 'api.php' to update route middleware and group authenticated routes

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-// post logout
-Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-// api resource product
-Route::apiResource('products', \App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
-
-// api resource order
-Route::apiResource('orders', \App\Http\Controllers\Api\OrderController::class)->middleware('auth:sanctum');
-
-// get categories
-Route::get('list-categories', [\App\Http\Controllers\Api\CategoryController::class, 'index'])->middleware('auth:sanctum');
-
-// api resource report
-Route::get('/reports/summary', [App\Http\Controllers\Api\ReportController::class, 'summary'])->middleware('auth:sanctum');
-Route::get('/reports/product-sales', [App\Http\Controllers\Api\ReportController::class, 'productSales'])->middleware('auth:sanctum');
-Route::get('/reports/close-cashier', [App\Http\Controllers\Api\ReportController::class, 'closeCashier'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::apiResource('products', \App\Http\Controllers\Api\ProductController::class);
+    Route::apiResource('orders', \App\Http\Controllers\Api\OrderController::class);
+    Route::get('list-categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
+    Route::get('/reports/summary', [App\Http\Controllers\Api\ReportController::class, 'summary']);
+    Route::get('/reports/product-sales', [App\Http\Controllers\Api\ReportController::class, 'productSales']);
+    Route::get('/reports/close-cashier', [App\Http\Controllers\Api\ReportController::class, 'closeCashier']);
+});
